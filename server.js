f2577d576f69b7ede6ac0f6be872e71cda4c05e3
Kaidhn Shearer <kaidhnshearer@gmail.com>
2024-08-09 13:00:01 +1000

    added path to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors());
